c164b960d8983ec2e126e43c469fd6e834c61353
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-03-17 20:19:44 +0530

    added webp of products in assests

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -5,6 +5,38 @@ import { v4 as uuid } from "uuid";
  * You can add products of your wish with different attributes
  * */
 
+//  {
+//   id: faker.random.uuid(),
+//   name: faker.commerce.productName(),
+//   image: faker.random.image(),
+//   price: faker.commerce.price(),
+//   material: faker.commerce.productMaterial(),
+//   brand: faker.lorem.word(),
+//   inStock: faker.random.boolean(),
+//   fastDelivery: faker.random.boolean(),
+//   ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+//   offer: faker.random.arrayElement([
+//     "Save 50",
+//     "70% bonanza",
+//     "Republic Day Sale"
+//   ]),
+//   idealFor: faker.random.arrayElement([
+//     "Men",
+//     "Women",
+//     "Girl",
+//     "Boy",
+//     "Senior"
+//   ]),
+//   level: faker.random.arrayElement([
+//     "beginner",
+//     "amateur",
+//     "intermediate",
+//     "advanced",
+//     "professional"
+//   ]),
+//   color: faker.commerce.color()
+// }
+
 export const products = [
   {
     _id: uuid(),
