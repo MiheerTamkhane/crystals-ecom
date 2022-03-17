08d1461457a96ce9582dca2aac59a3d65a8c985c
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-03-17 16:26:56 +0530

    made homepage changes

--- src/frontend/routes/Home/Home.js
+++ src/frontend/routes/Home/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
     {
       title: "Full Moon Collection",
       desc: " A curated collection of Crystals, chosen for the Full Moon. I time to reflect and let go.",
-      img: "/assets/images/prod1.jpg",
+      img: "/assets/images/land-product1.webp",
       left: true,
     },
     {
       title: "Crystal Jewellery",
       desc: "We have a beautiful range of jewellery to suit all budgets.",
-      img: "/assets/images/prod2.jpg",
+      img: "/assets/images/land-product2.webp",
       left: false,
     },
   ];
