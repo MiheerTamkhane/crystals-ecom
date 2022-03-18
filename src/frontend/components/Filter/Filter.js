d76e7520178e76f018251fc6af17598af2b35a53
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-03-18 20:55:47 +0530

    clear consoles

--- src/frontend/components/Filter/Filter.js
+++ src/frontend/components/Filter/Filter.js
@@ -5,7 +5,7 @@ import "./Filter.css";
 
 function Filter() {
   const { data, isMobileFilter, setIsMobileFilter } = useAccordion();
-  console.log("from filter : " + data[0]);
+
   return (
     <section
       className={
@@ -39,60 +39,10 @@ function Filter() {
       </div>
 
       {data.map((item, i) => (
-        <Accordion data={item} />
+        <Accordion key={i} data={item} />
       ))}
     </section>
   );
 }
 
 export default Filter;
-
-{
-  /* <div class="filter-comp">
-<h3 class="filter-heading">Category</h3>
-<ul class="list">
-  <li>
-    <input type="checkbox" id="all" />
-    <label for="all">All Jewellery</label>
-  </li>
-  <li>
-    <input type="checkbox" id="necklace" />
-    <label for="necklace">Necklace</label>
-  </li>
-  <li>
-    <input type="checkbox" id="rings" />
-    <label for="rings">Rings</label>
-  </li>
-</ul>
-</div>
-<div class="filter-comp">
-<h3 class="filter-heading">Rating</h3>
-<ul class="list">
-  <li>
-    <input type="radio" name="rate" id="fourstar" />
-    <label for="fourstar"> 4 Stars & above</label>
-  </li>
-  <li>
-    <input type="radio" name="rate" id="threestar" />
-    <label for="threestar">3 Stars & above</label>
-  </li>
-  <li>
-    <input type="radio" name="rate" id="twostar" />
-    <label for="twostar"> 2 Stars & above</label>
-  </li>
-</ul>
-</div>
-<div class="filter-comp">
-<h3 class="filter-heading">Sort by</h3>
-<ul class="list">
-  <li>
-    <input type="radio" name="sort" id="sortzero" />
-    <label for="sortzero">$50 to $100</label>
-  </li>
-  <li>
-    <input type="radio" name="sort" id="sortone" />
-    <label for="sortone">$100 to $200</label>
-  </li>
-</ul>
-</div> */
-}
