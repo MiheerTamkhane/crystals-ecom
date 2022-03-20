fa743b5e4b6f97396e6ce6712f0c17a3154e48db
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-03-20 09:51:15 +0530

    fix: routes of screens too

--- src/frontend/routes/Home/Home.js
+++ src/frontend/routes/Home/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Banner } from "../../components/index";
-
-import { Poster } from "../../components/index";
+import { Banner, Poster } from "../../components/index";
 
 const Home = () => {
   const homeData = [
