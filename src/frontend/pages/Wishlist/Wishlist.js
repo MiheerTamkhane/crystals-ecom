972dff51cb074e3a80ed3891a32b485855f2d423
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-08-08 22:31:51 +0530

    style : add `product-stats` class in `wishlist` page

--- src/frontend/pages/Wishlist/Wishlist.js
+++ src/frontend/pages/Wishlist/Wishlist.js
@@ -38,7 +38,7 @@ function Wishlist() {
                   favorite
                 </button>
                 <img src={image} alt="fist" className="ct-card-img" />
-                <div className="ct-product-stats">
+                <div className="ct-product-stats product-stats">
                   <h4>{name}</h4>
                   <small className="ct-product-info">{material}</small>
                   <small className="ct-product-info">{category}</small>
